9d6f47f87ac27b83256e03c9fe38e088fb065acd
Conrodot <connor.emery@yahoo.com>
2016-09-26 13:41:20 -0400

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,160 +1,294 @@
-//IMPORTS
-import java.io.*;
 import java.net.*;
+import java.util.Arrays;
+import java.io.*;
 
-/*
- * This class will create a send and receive Socket, Then create 5 read 
- * and 5 write packets, Next prints out the information of the packets,
- * The packet is then sent to port 23, Wait..., Finally it will print the 
- * received information 
- */
-public class Client {
-	
-	//INSTANCE VARIBLES
-	DatagramSocket snrSocket;
-	DatagramPacket sPacket;
-	DatagramPacket rPacket;
-	//String messages to bytes
-	String s1 = "TEXT.txt";
-	String s2 = "OcTEt";
-	byte[] b1 = s1.getBytes();
-	byte[] b2 = s2.getBytes();
-	byte[] msg = new byte[4 + b1.length + b2.length];
-	byte receiveData[] = new byte[4];
-	//general length integer
-	private int len;
-	
 
+
+public class Client 
+{
 	
-	public Client(){
-		
-		try {
-			//Create the send and receive Socket bound to any available 
-			//port
-			snrSocket = new DatagramSocket();
+	private DatagramSocket sendReceiveSocket;
+	private DatagramPacket sendPacket, receivePacket;
+	private String fileName;
+	private String mode;
+	private byte[] message;
+	
+	public Client()
+	{
+		// create DatagramSocket to use to both send and receive
+		try
+		{
+			sendReceiveSocket = new DatagramSocket();
 		}
-		
-		catch (SocketException sE) {
-			//Socket cannot be created
-			sE.printStackTrace();
+		catch(SocketException se)
+		{
+			se.printStackTrace();
 			System.exit(1);
 		}
+		fileName = "test.txt";
+		mode = "ocTEt".toLowerCase();
+		
+		byte[] data = new byte[4];
+		receivePacket = new DatagramPacket(data,data.length);
 	}
-	
-	/*This class loops 11 times creating 5 read request packets, 5 write 
-	 * request packets and 1 error packet.
+	/**
+	 * Algorithm for Client 
+	 * @return 
 	 */
-	public void snrPackets() {
+	public void createPack(byte[] packet){
+		try{
+			sendPacket = new DatagramPacket(packet,packet.length,InetAddress.getLocalHost(),23);
+		}
+		catch(UnknownHostException e){
+			e.printStackTrace();
+			System.exit(1);
+		}
+    	
+	}
+
+	public void ClientAlgorithm()
+	{
 		
-		//Repeat 11 times
-		for(int i = 0; i <= 11; i++) {
-			
-			//CREATING MESSAGE FOR PACKET
-			msg[0] = 0;
-			//Invalid request packet is made    msg = (0 0)
-			if(i == 10) {  
-				msg[1] = 0;
-			}
-			//read request packet is made        msg = (0 1)
-			else if(i%2 == 0) {
-				msg[1] = 1;
-			}
-			//Write request packet is made       msg = (0 2)
-			else {
-				msg[1] = 2;
-			}
-			
-			//Complete msg format
-			System.arraycopy(b1, 0, msg, 2, b1.length);
-			msg[b1.length + 2] = 0;
-			
-			System.arraycopy(b2, 0, msg, b1.length + 3, b2.length);
-			msg[3 + b1.length + b2.length] = 0;
-			
-			//SENDING PACKETS
-			//The client and server are on the same computer.
-			//The destination port number on the destination host (23).
-			try {
-				sPacket = new DatagramPacket(msg, msg.length,
-											 InetAddress.getLocalHost(),
-											 23);
-			}
-			catch(UnknownHostException e) {
-				e.printStackTrace();
-				System.exit(1);
-			}
-			
-			//Print the information put in the packet
-			printSPacket();
+		String request;
 		
-			//Send
-			try {
-				snrSocket.send(sPacket);
+		for(int i = 0; i < 11; i++)
+		{
+			message = new byte[4 + fileName.length() + mode.length()];
+			message[0] = 0;
+			
+			if(i == 10)
+			{
+				message[1] = 0;
+				request = "Error"; // #11 invalid request
 			}
-			catch(IOException e) {
+			else if(i%2 == 0)
+			{
+				message[1] =1;
+				request = "Read"; // read request
+			}
+			else
+			{
+				message[1]=2;
+				request = "Write"; // write request
+			}
+			
+			byte[] fileNameToBytes = fileName.getBytes();
+			int os1 = fileNameToBytes.length;
+			
+			try{
+				File file = new File(fileName);
+				
+				if(file.createNewFile()){
+					System.out.println("File is created");
+				}else{
+					System.out.println("File already exists.");
+				}
+				
+			}catch (IOException e){
+				e.printStackTrace();
+			}
+			
+			System.arraycopy(fileNameToBytes, 0, message, 2, os1);
+			message[os1 + 2] = 0;
+			
+			byte[] modeToBytes = mode.getBytes();
+			int os2 = modeToBytes.length;
+			
+			System.arraycopy(modeToBytes, 0, message, os1 + 3, os2);
+			int os3 = os1 + os2 + 3;
+			message[os3] = 0;
+			
+			// forming Packet that will be sent to server but first the intermediate host at port 23
+			try
+			{
+				sendPacket = new DatagramPacket(message,message.length,InetAddress.getLocalHost(),23);
+			}
+			catch(UnknownHostException e)
+			{
 				e.printStackTrace();
 				System.exit(1);
 			}
-			System.out.println("Client: Packet sent to host :)");
 			
-			//RECEIVING PACKETS
-			//A receiving packet is constructed to receive packets with
-			//size msg
-			rPacket = new DatagramPacket(receiveData, 4);
-			//datagram blocked until Socket receives msg.
-			try {
-				snrSocket.receive(rPacket);
+			
+			
+			send(sendPacket);
+			
+			
+			
+			//Send Packet info
+			
+			
+			System.out.println("Sending: " + request + " Request");
+			System.out.println("Host: " + sendPacket.getAddress());
+			System.out.println("Destination Port: " + sendPacket.getPort());
+			System.out.println("File Name: " + fileName);
+			System.out.println("Mode: " + mode);
+			
+			int length1 = os3 + 1;
+			System.out.println("Length: " + length1);
+			String info = new String(message,0,length1);
+			System.out.println("String : " + info);
+			System.out.println("Bytes : " + Arrays.toString(message));
+			
+			// receive packet
+			
+			
+			
+			try
+			{
+				sendReceiveSocket.receive(receivePacket);
 			}
-			catch(IOException e) {
+			catch(IOException e)
+			{
 				e.printStackTrace();
 				System.exit(1);
 			}
 			
-			//Print the received datagram
-			printRPacket();
-			System.out.print("Containing: ");
-		    // Form a String from the byte array.
-		    String received = new String(receiveData,0,len);   
-		    System.out.println(received);
+			// Receive Packet info
+			System.out.println("Client received packet");
+			System.out.println("Sent from Host: " + receivePacket.getAddress());
+			System.out.println(" using port: " + receivePacket.getPort());
+			int length2 = receivePacket.getLength();
+			System.out.println("Length: " + length2);
+			System.out.println("Packet: ");
+			
+			for(int k = 0; k<receivePacket.getData().length;k++)
+			{
+				System.out.print(" " + receivePacket.getData()[k]);
+			}
+			
+			byte[] compWrite = new byte[] {0,3,0,1};
+			
+			if(receivePacket.getData() == compWrite){
+				System.out.println("files match");
+				
+			}
 		}
 		
-		//All done
-		snrSocket.close();
+	}
+	public void send(DatagramPacket sendPacket){
+		
+		try{
+			sendReceiveSocket.send(sendPacket);
+		}
+		catch(IOException e){
+			e.printStackTrace();
+			System.exit(1);
+		}
+		System.out.println("Client has sent packet\n");
+		
+	}
+	public void receive (){
+		try
+		{
+			sendReceiveSocket.receive(receivePacket);
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
 	}
 	
-	/*This class prints all the current information of put into the 
-	 *send packet
-	 */
-	public void printSPacket() {
-		len = sPacket.getLength();
-		System.out.println("Client: Sending packet");
-	    System.out.println("To host: " + sPacket.getAddress());
-	    System.out.println("Destination host port: " + sPacket.getPort());
-	    System.out.println("Length: " + len);
-	    System.out.print("Containing: ");
+    public void sendData(String name)
+    	throws FileNotFoundException, IOException
+        {
+    		int packNum = 0;
+        	BufferedInputStream in = new BufferedInputStream(new FileInputStream("test.txt"));
+        	//bytes from file
+            byte[] fdata = new byte[512];
+            //op code and block # + fdata
+            byte[] pack = new byte[516];
+            
+            int n;
+            int a;
+            int b;
+            pack[0] = 0;
+            pack[1] = 3; 
+            while ((n = in.read(fdata)) != -1) {
+            	//System.out.println("boop");
+            	pack[3] = (byte) (packNum & 0xFF);
+            	pack[2] = (byte) ((packNum >> 8) & 0xFF); 
+            	packNum ++;
+            	
+            	// WRITE LATER/////////////////////////////////
+            	//System.out.println(pack[515]);
+            	if (fdata[511] == 0x00){
+            		//for (lenTest = 4; pack[lenTest] != 0x00; lenTest++){
+            		//}
+            		
+            	    byte[] lastData = resize(fdata);
+            	    System.out.println(lastData[3]);
+            		System.out.println("data not 512 bytes");
+            		System.out.println("Size of this is array is: " + lastData.length);
+            		System.arraycopy(lastData, 0, pack, 4, lastData.length);
+            		byte[] lastPack = resize(pack);
+            		createPack(lastPack);
+            		 a = lastPack[2];
+                	 b = lastPack[3];
+                	a &= 0xFF;
+                	b &= 0xFF;
+                	System.out.println(lastPack[4]);
+            	}
+            	else{
+            	System.arraycopy(fdata, 0, pack, 4, fdata.length);
+            	createPack(fdata);
+            	
+            	///////////////////////////////////////////////
+            	 a = pack[2];
+            	 b = pack[3];
+            	a &= 0xFF;
+            	b &= 0xFF;
+            	}
+            	System.out.println(a + ", " + b);
 
-	    for(int j = 0; j < msg.length; j++){
-	    	System.out.print(msg[j] + " ");
-	    }
-	    System.out.println();
-	}
-	
-	/*This class prints all the current information of put into the 
-	 *received packet
-	 */
-	public void printRPacket() {
-		len = rPacket.getLength();
-		System.out.println("Client: Packet received:");
-	    System.out.println("From host: " + rPacket.getAddress());
-	    System.out.println("Host port: " + rPacket.getPort());
-	    System.out.println("Length: " + len);
-	}
-	
-	
-	public static void main(String args[])
+            	
+            	
+            	send(sendPacket);
+            	re(fdata);
+            	//receive();
+            	
+            }
+
+            in.close();
+        }
+		
+    public byte [] re (byte[] data){
+    	for (int i = 0; i < data.length; i++){
+    		data[i] = 0x00;
+    	}
+    	return data;
+    }
+    
+    public byte[] resize (byte[] data){
+    	int i;
+    	for(i = 4; i < data.length; i++){
+    		if(data[i] == 0x00){
+    			break;
+    			
+    		}
+    	}
+    	
+    	data = Arrays.copyOf(data, i+1);
+    	
+    	return data;
+    }
+    
+	public static void main(String[] args) 
 	{
-		Client c = new Client();
-		c.snrPackets();
-	}
 
+		Client c = new Client();
+		//c.ClientAlgorithm();
+		try {
+			c.sendData("test.txt");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
